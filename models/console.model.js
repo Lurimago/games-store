e18a76e32b367dfa227df33998e2eae727391ab0
Luis Ricardo Marcelo Gómez <luisricardomarcelogomez@gmail.com>
2022-09-22 22:55:35 -0500

    Update console.model.js

--- models/console.model.js
+++ models/console.model.js
@@ -15,6 +15,11 @@ const Console = db.define('console', {
 		type: DataTypes.STRING,
 		allowNull: false,
 	},
+	status: {
+		type: DataTypes.STRING,
+		allowNull: false,
+		defaultValue: 'active',
+	},
 
 });
 
